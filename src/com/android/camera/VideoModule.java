6459a48ec2c53fe714d0381b732793b39adab286
Zhang Gengen <gengenx.zhang@intel.com>
2013-01-31 18:02:56 +0800

    [PORT FROM MAIN] Android Default Camera: fix preview screen is shown after recording a video in messaging
    BZ: 81952
    
    Issue:
    preview will not show the recorded view after stop recording
    
    Root Causes:
    When the video stop recording, it invoked unlockOrientation method from other app by intent
    
    Solution:
    video not invoke unlockOrientation after stop recording.
    
    Change-Id: Ifadc3a07bedcbff98c7caebfa68bc4766b196234
    Orig-Change-Id: I4ce3f0f0b7230410213f9a108eae2cc9f442d5fe
    Category: feature differentiation
    Domain: camera
    Origin: internal
    Upstream-Candidate: no
    Signed-off-by: Zhang Gengen <gengenx.zhang@intel.com>

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1726,7 +1726,9 @@ public class VideoModule implements CameraModule,
                 fail = true;
             }
             mMediaRecorderRecording = false;
-            mActivity.getOrientationManager().unlockOrientation();
+            if (!mIsVideoCaptureIntent) {
+                mActivity.getOrientationManager().unlockOrientation();
+            }
 
             // If the activity is paused, this means activity is interrupted
             // during recording. Release the camera as soon as possible because
