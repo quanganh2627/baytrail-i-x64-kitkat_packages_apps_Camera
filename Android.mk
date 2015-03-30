ce8989ced8a2c62b697e75dcf6e53516548dbac4
guochaox <guochaox.su@intel.com>
2015-03-30 11:39:20 +0800

    enable MultiCameraApp and MultiCamera2 auto include and build
    Change-Id: I25a206ed2e7ca741a6cc4b11db97ea7086fa80fa
    Signed-off-by: guochaox <guochaox.su@intel.com>
    Reviewed-on: https://android.intel.com/347980
    Reviewed-by: Zhang, Clear <clear.zhang@intel.com>
    Tested-by: Zhang, Clear <clear.zhang@intel.com>
    Reviewed-on: https://android.intel.com/351774
    Reviewed-by: Anuar, Nuhairi <nuhairi.anuar@intel.com>
    Tested-by: Anuar, Nuhairi <nuhairi.anuar@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
+ifeq ($(USE_ISG_MULTI_CAMERA_ALL_SHOW), true)
+    include $(TOP)/packages/apps/Camera/MultiCamera/Android.mk
+endif
+ifeq ($(USE_ISG_MULTI_CAMERA_ONE_SHOW), true)
+    include $(TOP)/packages/apps/Camera/MultiCamera2/Android.mk
+endif
 # leaving the makefile emtpy to prevent the build
 # system from traversing the project
